eb2d0f44c49bc3d2dc5e6c5d40bf996f1f6e682a
Andréia Ribeiro <asribeirodesign@gmail.com>
2021-05-19 16:42:43 +0100

    ScreenNewExpenses

---
+++ ScreenNewExpenses.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text, StatusBar, Dimensions, TextInput, TouchableOpacity, Image, Flaylist } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+import React from 'react';
+import { Text } from 'react-native';
+export default function ScreenNewExpenses({ navigation }) {
+
+
+    return (
+        <>
+
+            <View style={styles.container}>
+
+                <Image
+                    source={require("./assets/SplitrLogo.png")}
+                    style={{ width: 200, height: 200, marginTop: 64 }}
+                    resizeModo="contain"
+                />
+
+
+                <TouchableOpacity
+
+                    onPress={() => {
+                        navigation.navigate('NewExpenses')
+                    }}
+
+                    style={styles.loginButton}
+                >
+                    <Text style={styles.textButton}>
+                        New Expense
+                        </Text>
+                </TouchableOpacity>
+
+                <FlatList //id="result"
+                ></FlatList>
+
+                <Text
+                > Expense here
+                     {expense} </Text>
+
+            </View>
+        </>
+    )
+};
+const width = Dimensions.get('window').width - 50;
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    header: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#1e1e1e',
+        paddingLeft: 25,
+        paddingTop: 20,
+
+    },
+    loginButton: {
+        width: 300,
+        height: 42,
+        backgroundColor: '#1e1e1e',
+        marginTop: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width,
+
+    },
+    textButton: {
+
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#FFF',
+
+    },
+    textInput: {
+        borderWidth: 2,
+        borderColor: '#1e1e1e',
+        color: '#1e1e1e',
+        width,
+        height: 50,
+        borderRadius: 10,
+        paddingLeft: 15,
+        fontSize: 25,
+        marginBottom: 15,
+
+    },
+    textInput2: {
+        borderWidth: 2,
+        borderColor: '#1e1e1e',
+        color: '#1e1e1e',
+        width,
+        height: 50,
+        borderRadius: 10,
+        paddingLeft: 25,
+        fontSize: 25,
+        marginBottom: 15,
+        marginTop: 15,
+    }
+
+});
